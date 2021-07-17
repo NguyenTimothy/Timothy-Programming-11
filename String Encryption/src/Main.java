30b7dc9aadda84d3aa728e2dfdad1f0a729f7890
NguyenTimothy <87158137+NguyenTimothy@users.noreply.github.com>
2021-07-17 09:38:41 -0700

    String Encryption

--- String Encryption/src/Main.java
+++ String Encryption/src/Main.java
@@ -2,26 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String name;
+        String str;
         String option;
         Scanner scan = new Scanner(System.in);
         System.out.println("Would you like to encrypt or decrypt? 'e' for encrypt and 'd' for decrypt.");
         option = scan.next();
-        if (!option.equals("e") && !option.equals("d")) {
-            System.out.println("Invalid entry. Please try again");
-        } else if (option.equals("e")) {
+        if (option.equals("e")) {
             System.out.println("Please enter the string you would like to encrypt:");
-            name = scan.next();
-            System.out.println(name.substring(name.length() - 1) + name.substring(0, name.length() - 1) +
-                    "ay\nOriginal: " + name);
-        } else {
+            str = scan.next();
+            // Moves the last letter to the front and adds 'ay' to the end
+            System.out.println(str.substring(str.length() - 1) + str.substring(0, str.length() - 1) +
+                    "ay\nOriginal: " + str);
+        } else if (option.equals("d")) {
             System.out.println("Please enter the string you would like decrypt:");
-            name = scan.next();
-            if (name.endsWith("ay")){
-                System.out.println(name.substring(1, (name.length() - 2)) + name.charAt(0) + "\nOriginal: " + name);
+            str = scan.next();
+            if (str.endsWith("ay")){
+                // Moves the first letter back the end and removes the 'ay'
+                System.out.println(str.substring(1, (str.length() - 2)) + str.charAt(0) + "\nOriginal: " + str);
             } else {
-                System.out.println("Invalid entry. Please try again.");
+                // If the string doesn't end in 'ay' then it can't be decrypted by this program
+                System.out.println("Cannot decrypt. Please try again.");
             }
+        } else {
+            // If the original option for encryption or decryption was neither 'e' nor 'd'
+            System.out.println("Invalid entry. Please try again.");
         }
     }
 }
