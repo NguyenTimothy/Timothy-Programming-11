660406c9746440f1d03ab8a79107482f11f5b9c7
NguyenTimothy <87158137+NguyenTimothy@users.noreply.github.com>
2021-07-17 09:38:20 -0700

    ProjectNumber1

--- ProjectNumber1/src/Main.java
+++ ProjectNumber1/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     static int problemOne(String s){
         int answer = 0;
         //your code here
+        // Checks every index position in a string if it is a vowel and raises 'answer' by one accordingly then returns 'answer'
         for (int i = 0; i < s.length(); i++) {
             if ((s.charAt(i) == 'a') || (s.charAt(i) == 'e') || (s.charAt(i) == 'i') ||
                     (s.charAt(i) == 'u') || (s.charAt(i) == 'o')) {
@@ -13,8 +14,10 @@ public class Main {
     }
     //Code you problem number two here
     static int problemTwo(String s){
-        int  answer = 0;
+        int answer = 0;
         //your code here
+        /* Checks every index position in a string if the value and the next two values spell out 'bob'
+        Raises 'answer' by one if it does then returns 'answer' */
         for (int i = 0; i < s.length(); i++) {
             if (s.startsWith("bob", i)){
                 answer++;
@@ -24,20 +27,30 @@ public class Main {
     }
     //Code your solution to problem number 3 here
     static String problemThree(String s){
+        //your code here
+        // Returns the longest substring of a string in which letters occur in alphabetical order
         String answer = "";
         for (int i = 0; i < s.length(); i++) {
+            /* Creates a string that starts at 'i' and ends in the same place as the original string.
+            The string gets smaller by one every loop by removing the front letter.
+            Creates a placeholder string that will be used later. */
             String substring = s.substring(i),
                     newString = "";
             char last = 0;
             for (int j = 0; j < substring.length(); j++) {
+                /* If the letter in the index position is equal or occurs later in the alphabet over the previous,
+                the program amends it to the placeholder string */
                 char c = substring.charAt(j);
                 if (last <= c) {
                     newString = newString.concat(Character.toString(c));
                     last = c;
                 } else {
+                    // Breaks out of the nested loop if the character occurs earlier in the alphabet over the previous
                     break;
                 }
             }
+            /* In the first run, 'answer' is assigned the value of the placeholder string and the program loops back.
+            In the next runs, if the new placeholder value is longer than the previous, 'answer' is assigned the new string. */
             if (newString.length() > answer.length()) {
                 answer = newString;
             }
@@ -54,5 +67,7 @@ public class Main {
         all possible cases.  If you have 100% success then there is no bugs in your methods.
          */
         String s;
+        s = "bobibobafghjlpbob";
+        System.out.println(problemOne(s) + "\n" + problemTwo(s) + "\n" + problemThree(s));
     }
 }
