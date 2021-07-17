194e005fdabfc074df248055c088dfaa3ebb58f1
NguyenTimothy <87158137+NguyenTimothy@users.noreply.github.com>
2021-07-16 19:14:37 -0700

    Add, Delete, Insert Arrays

--- Add, Delete, Insert Arrays/src/Main.java
+++ Add, Delete, Insert Arrays/src/Main.java
@@ -1,40 +1,48 @@
 public class Main {
-    static int[] addArray(int[] a, int b){
-        // Adds an integer to the end of an array of integers. The array is 'a' and the integer being added is 'b'
-        int[] c = new int[a.length + 1];
-        System.arraycopy(a, 0, c, 0, a.length);
-        c[a.length] = b;
-        return c;
+    static int[] addArray(int[] array, int integer){
+        // Adds an integer to the end of an array of integers.
+        int[] newArray = new int[array.length + 1];
+        for (int i = 0; i < array.length; i++)
+            newArray[i] = array[i];
+        newArray[array.length] = integer;
+        return newArray;
     }
 
-    static int[] delArray(int[] a){
-        // Deletes the last integer in an array of integers. 'a' is the array.
-        int[] b = new int[a.length -1];
-        System.arraycopy(a, 0, b, 0, a.length - 1);
-        return b;
-    }
-
-    static int[] insertArray(int[] a, int b, int c) {
-        // Inserts an integer in an array of integers. 'a' is the array, 'b' is the integer, 'c' is the index position
-        int[] d;
-        if (c <= a.length) {
-            d = new int[a.length + 1];
-            System.arraycopy(a, 0, d, 0, c);
-            d[c] = b;
-            System.arraycopy(a, c, d, c + 1, a.length - c);
-        } else {
-            d = new int[c + 1];
-            System.arraycopy(a, 0, d, 0, a.length);
-            d[c] = b;
+    static int[] delArray(int[] array){
+        // Deletes the last integer in an array of integers.
+        int[] newArray = new int[array.length - 1];
+        for (int i = 0; i < newArray.length; i++){
+            newArray[i] = array[i];
         }
-        return d;
+        return newArray;
+    }
+
+    static int[] insertArray(int[] array, int integer, int index) {
+        // Inserts an integer in an array of integers.
+        int[] newArray;
+        if (index <= array.length) {
+            newArray = new int[array.length + 1];
+            for (int i = 0; i < index; i++){
+                newArray[i] = array[i];
+            }
+            for (int i = index + 1; i < array.length; i++){
+                newArray[i] = array[i];
+            }
+        } else {
+            newArray = new int[index + 1];
+            for (int i = 0; i < array.length; i++){
+                newArray[i] = array[i];
+            }
+        }
+        newArray[index] = integer;
+        return newArray;
     }
 
     public static void main(String[] args) {
         // Example of the addArray method
         int[] addEg = {10, 11, 12, 13, 14, 15};
         addEg = addArray(addEg, 16);
-        for(int i: addEg){
+        for (int i: addEg){
             System.out.println(i);
         }
         System.out.println("\n");
@@ -42,7 +50,7 @@ public class Main {
         // Example of the delArray method
         int[] delEg = {20, 21, 22, 23, 24, 25};
         delEg = delArray(delEg);
-        for(int i: delEg){
+        for (int i: delEg){
             System.out.println(i);
         }
         System.out.println("\n");
@@ -51,7 +59,7 @@ public class Main {
         int[] insertEg = {30, 31, 32, 34, 35, 36};
         insertEg = insertArray(insertEg, 33, 3);
         insertEg = insertArray(insertEg, 37, 10);
-        for(int i: insertEg){
+        for (int i: insertEg){
             System.out.println(i);
         }
     }
