4947af789828ae119d4542bb795b3045d57bb695
NguyenTimothy <87158137+NguyenTimothy@users.noreply.github.com>
2021-07-17 17:22:07 -0700

    Drawing With Text

---
+++ Drawing With Text/src/Main.java
@@ -0,0 +1,91 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                                                          `+syso/,\s
+                                 /ss+-                    +sddddhso:\s
+                                .shdhs+`                 `syddddddhyo-\s
+                                .shdddys-                .shddddddddhs+.\s
+                    ````````````.shddddys:```````````````.shddddddhhddys:\s
+                ```.........`..`.oyddhhdhs/...---:+++-`.``ohdddddddhyddhs/`\s
+             ```...........`..`..+shdddddhs+----:ooo:..`.`/yddddddddhyddhs+`\s
+           ```............`...`..:sddddddhy:---:ooo/...`.`.sdddddddddyhddds+\s
+          ``.............`...`....sydddhhy:---:oooo-......`-ddddddddddsdddhs:\s
+        ``...............`..`.....+shhhdy:----+oooo.....`..`:hddddddddhydddyo`\s
+       ``...............`...`.....-shddh:----/oooo+......`..`.+ydddddddydddhs:\s
+      ``................`...`......oydd/----:oooooo.......`...`.:+yhdddydddds+\s
+      `........`.......`...`.......:sdo-----/oooooo-.......`.....`..-:/+ssyyo+\s
+     ``.......``.......`...`........oo:-----+oooooo+........`...........```-/+\s
+    ``.......```.......`...`........::-----:oooooooo/........`.........``-ohy+\s
+    ``......``/`.......`...`.........------:ooooooooo/.........`....``..-hdhs-\s
+    `.......`:s``......`............-------:oooooooooo+-......`..-/sho-oddh:/\s
+   ``.......`oy-`.......`.`.........-------:+++++///;:::```````:ymMMMMhoo+::`\s
+   ``.......`yd+`.......`........`...-----::///+++oossyy    `````-omMMMdoys+`\s
+   ``.......`hdh.`......``.``.-:+osyyhhddddddddddddddddy ``.yddhs/.-yMMMNdys:\s
+   ``.......`ydds`..`..:+/-/oyyso++yddddddddddddddddddds ` sMMMMMMd+-sMMMNdss`\s
+   ``.......`sddd/./+-sh/+mMMMMms+. -sdddddddddddddddddy   -NMMMMMMMh:sMMMdyo:\s
+   ``.......`/hddshddho.oMMMMm/`      :hdddddddddddddddd`   -dMMMMMMMo:dMMh+//,\s
+   ``....``` -sydh++oss`NMMMN-````:sys+-sddddddddddddddd:    `/yNNMNm+-+MMN+//`\s
+    `.```     .+shdhyss-MMMMo````-MMMMMN/odddddddddddddds   `hho`-:-`-/+NMNsss.\s
+    ``          .+osyys-MMMM:....`mMMMMMM/+dddddddddddddd-   +hd.    .ooNMMs/o.\s
+                  `.:/osmMMM:..-` .dMMMMMM/odddddddddddddy`          .mmMMNdso.\s
+                    `oshmMMMo---`   /hMMMMM:ydddddddddddddo          `sdMMmdss\s
+                     -ssdNMMm:--`  :so.-//- -dddddddddddddd/         ./NMmdhs/\s
+                      /sydMMMs---  :dMd      odddddhhhhhdddd/       .oNNdddss`\s
+                       ///oNMMs//-           .ddddhdddddddddd+:...:odmddddys:\s
+                        ///oNMMhoo/           ydddddddddddhddddddddddddddys/\s
+                         -ss/dMMNmms`         odddddddddddydddddddddddddys/\s
+                          -o:hdNMMMh/.`     .+ddddyhddddddddhhddddddddhso-\s
+                           `:oyhdmNNNmhsoooydddddddddddddddhhddddddddys/`\s
+                            `./oyhddddddddddddddddhhhhhhhhhdddddddhyo/-.``\s
+                           ``...:+syhddddddddddddddddddddddddddhyyy+`...``\s
+                          ``.....`.:+oyhhddddddddddddddddddhhys/://``...``\s
+                         ``.....``....-:/+osyyyhhhhhhyyyyyyhhd-`.......`.``\s
+                         `......`........-:+sshhhhhhhhdddddddd``......`...`\s
+                        ``.....``......./osyssdddddddddddddddh``........-:`\s
+                        ``.....`......-+syyyysdddddddddddddddh``.......-:o-`\s
+                        `......`.....-+syshdhsdddddddddddddddd``.....--:+o-``\s
+                       ``.....``.....+syshdddsdddddddddddddddd-`..----:+o+.``\s
+                       ``.....`...../oyshddddhdddddddddddddddd+`-----:+oo:..`\s
+                       `......`.....+sshddddddddddddddddddddddh..--:/ooo+...`.\s
+                      ``.....``....-+ysdddddddddddddddddddddddd+`-/+ooo+-..`.+\s
+                      ``.....`.....:osyddddddddddddddddddddddddd-.oooo+-...`+s\s
+                      `.....``.....:osydddddddddddddddddddddddddy.:oo/....`/ys\s
+                     ``.....`......:oyydddddddddddddddddddddddddds`--....`+dyo\s
+                     ``....``......-+ysdddddddddddddddddddddddddddo``..`.sdds/\s
+                     `.....`........+ssyddddddddddddddddddddddddddds.`./hddys`\s
+                    ``....`.........:oyshddddydddddddddddddddddddddds/ydddhs:\s
+                    `....``.........-+yyshdddsdddddddddddddddddddddddsdddhs:\s
+                   ``....`..........-/ohhsydhsdddddddddhhddhdddddddddshdyo-\s
+                   `....`..........---+shhyssyddddddddddhsyddddddddddyss/`\s
+                  ``...``.........---:/ohhhhshddddddddddhshddddddddddhs/\s
+                  `....`..........-/+ssoyhhyshddddddddddhshdddddddddddyo\s
+                 ``...`..........-+shhhhyhhysdddddddddddhshdddddddddddhs.\s
+                ``...`..........-/ohhhhhhhhsydddddddddddhshdddddddddddds/\s
+                `...``.........-/ohhhhhhhhhshdddddddddddhsyddddddddddddyo\s
+               ``..``.........-:+yhhhhhhhhysddddddddddddhsyddddddddddddhs.\s
+              ``..``.........--+ohhhhhhhhhysdddddddddddddsyddddddddddddds/\s
+              `...`.........--:+yhhhhhhhhhsydddddddddddddsydddddddddddddys\s
+             ``..`.........---+shhhhhhhhhhshdddddddddddddssdddddddddddddhs-\s
+            ``..`.........---:+yhhhhhhhhhysddddddddddddddssdddddddddddddds+\s
+           ``..`.........----/ohhhhhhhhhhysddddddddddddddssddddddddddddddys`\s
+          ``..`.........-----+shhhhhhhhhhsyddddddddddddddssddddddddddddddhs:\s
+          `..`.........-----:+yhhhhhhhhhhshddddddddddddddssddddddddddddddds+\s
+         `..`........-------/ohhhhhhhhhhyshddddddddddddddssdddddddddddddddys`\s
+        `.``........--------/ohhhhhhhhhhysdddddddddddddddsshddddddddddddddds:\s
+       `.``........---------+ohhhhhhhhhhsydddddddddddddddsshdddddddddddddddyo\s
+     ``.``.......-----------+shhhhhhhhhhshdddddddddddddddyshdddddddddddddddhs.\s
+    ``.``.......-----------./shhhhhhhhhhshdddddddddddddddyshdddddddddddddddds:\s
+   ````........---...`````  /ohhhhhhhhhysddddddddddddddddysyddddddddddddddddyo\s
+  ```````````````           /ohhhhhhhhhyyddddddddddddddddysyddddddddddddddddhs.\s
+                            :ohhhhhhhhhsyddddddddddddddddysyddddddddddddddddds/\s
+                            -+yhhhhhhhhshddddddddddddddddysydddddddddddddddddyo\s
+                            `+shhhhhhhysdddddddddddddddddysydddddddddddddddddhs.\s
+                             /ohhhhhhhyydddddddddddddddddhsydddddddddddddddddds/\s
+                             `/syhhhhhsydddddddddddddddddhsyddddddddddddddddddyo\s
+                              `-/+ooooshdddddddddddddddddhssddddddddddddddddddhs`\s
+                                   ``-shdddddddddddddddddhssddddddddddddddddddds-\s
+                                      oyddddddddddddddddhs/oydddddddddddddddddyo`\s
+                                      `-+osyyyyyyyyyyyso/- `:+syyyyyyyyyyyyys+:`\s
+                                           ``.......``         `..........``\s""");
+    }
+}
