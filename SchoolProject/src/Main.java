36bf6db092679630d6e1fb432a9562e9a47584e9
NguyenTimothy <87158137+NguyenTimothy@users.noreply.github.com>
2021-07-27 10:00:37 -0700

    SchoolProject

--- SchoolProject/src/Main.java
+++ SchoolProject/src/Main.java
@@ -2,16 +2,16 @@ public class Main {
     public static void main(String[] args) {
         School school = new School("Timothy Nguyen Secondary School", 100, true);
 
-        school.addStudent("Michelle", "Thuy", 12);
-        school.addStudent("Leslie", "Perez", 11);
+        school.addStudent("Michelle", "Thuy", 8);
+        school.addStudent("Leslie", "Perez", 12);
         school.addStudent("Dylon", "Walters", 11);
         school.addStudent("Allana", "Tran", 10);
         school.addStudent("Billy", "Kwan", 9);
-        school.addStudent("Joe", "Johnson", 10);
+        school.addStudent("Joe", "Johnson", 12);
         school.addStudent("Justin", "White", 8);
         school.addStudent("Cindy", "Ford", 10);
-        school.addStudent("Dylan", "Black", 7);
-        school.addStudent("Carly", "Morrison", 7);
+        school.addStudent("Dylan", "Black", 9);
+        school.addStudent("Carly", "Morrison", 11);
 
         school.addTeacher("David", "Smith", "English");
         school.addTeacher("Bill", "Pham", "Math");
